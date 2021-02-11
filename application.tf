1c410c01c1e7aa8a63c9ecefaadeb81f9d16dc29
Conor Murphy <conmurph@cisco.com>
2021-02-11 23:17:52 +0100

    Adding application 2 EPGs into tenant:conmurph

--- application.tf
+++ application.tf
@@ -3,7 +3,7 @@ data "aci_tenant" "conmurph" {
   name  = "conmurph"
 }
 
-/*resource "aci_application_profile" "application_2" {
+resource "aci_application_profile" "application_2" {
   tenant_dn  = "${data.aci_tenant.conmurph.id}"
   name       = "application_2"
 }
@@ -23,4 +23,4 @@ resource "aci_subnet" "application_2" {
   parent_dn                    = "${aci_bridge_domain.application_2.id}"
   ip                                  = "2.2.2.252/24"
   description                         = "This subject is created by terraform"
-}*/
+}
